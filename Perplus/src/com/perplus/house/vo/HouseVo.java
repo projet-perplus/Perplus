1dfa47609523363f94d4853ecc732d51d7c2034c
hojin Lee <lhj8770@gmail.com>
2016-11-30 20:11:37 +0900

    HouseVo.java 수정

--- Perplus/src/com/perplus/house/vo/HouseVo.java
+++ Perplus/src/com/perplus/house/vo/HouseVo.java
@@ -1,5 +1,15 @@
 package com.perplus.house.vo;
 
-public class HouseVo {
+import java.io.Serializable;
+import java.sql.Timestamp;
 
+public class HouseVo implements Serializable{
+	private String memberEmail;
+	private int HouseSerial;
+	private int houseRating;
+	private String houseTitle;
+	private String houseContent;
+	private Timestamp checkinStart;
+	private Timestamp checkinEnd;
+	private String houseNecessaryCondition;
 }
