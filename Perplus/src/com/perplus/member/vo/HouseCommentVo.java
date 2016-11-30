88eb2f45399e994010e91d4b3430646eb69be797
sangyoeb choi <ask1302@naver.com>
2016-11-30 21:22:04 +0900

    member.vo 6개 commit

--- Perplus/src/com/perplus/member/vo/HouseCommentVo.java
+++ Perplus/src/com/perplus/member/vo/HouseCommentVo.java
@@ -1,5 +1,106 @@
 package com.perplus.member.vo;
 
-public class HouseCommentVo {
+import java.util.Date;
 
+import com.perplus.house.vo.HouseVo;
+
+public class HouseCommentVo {
+	private int houseSerial;//하우스번호 번호
+	private String memberEamil;//이메일
+	private String commentContent;//내용
+	private int commentRating;//별점
+	private Date commentTime;//등록일자
+	private MemberVo member;//
+	
+	
+	public HouseCommentVo(int houseSerial, String memberEamil, String commentContent, int commentRating,
+			Date commentTime) {
+		this.houseSerial = houseSerial;
+		this.memberEamil = memberEamil;
+		this.commentContent = commentContent;
+		this.commentRating = commentRating;
+		this.commentTime = commentTime;
+	}
+	
+	public HouseCommentVo() {}
+	
+	
+	public int getHouseSerial() {
+		return houseSerial;
+	}
+	public void setHouseSerial(int houseSerial) {
+		this.houseSerial = houseSerial;
+	}
+	public String getMemberEamil() {
+		return memberEamil;
+	}
+	public void setMemberEamil(String memberEamil) {
+		this.memberEamil = memberEamil;
+	}
+	public String getCommentContent() {
+		return commentContent;
+	}
+	public void setCommentContent(String commentContent) {
+		this.commentContent = commentContent;
+	}
+	public int getCommentRating() {
+		return commentRating;
+	}
+	public void setCommentRating(int commentRating) {
+		this.commentRating = commentRating;
+	}
+	public Date getCommentTime() {
+		return commentTime;
+	}
+	public void setCommentTime(Date commentTime) {
+		this.commentTime = commentTime;
+	}
+	@Override
+	public String toString() {
+		return "HouseCommentVo [houseSerial=" + houseSerial + ", memberEamil=" + memberEamil + ", commentContent="
+				+ commentContent + ", commentRating=" + commentRating + ", commentTime=" + commentTime + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((commentContent == null) ? 0 : commentContent.hashCode());
+		result = prime * result + commentRating;
+		result = prime * result + ((commentTime == null) ? 0 : commentTime.hashCode());
+		result = prime * result + houseSerial;
+		result = prime * result + ((memberEamil == null) ? 0 : memberEamil.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HouseCommentVo other = (HouseCommentVo) obj;
+		if (commentContent == null) {
+			if (other.commentContent != null)
+				return false;
+		} else if (!commentContent.equals(other.commentContent))
+			return false;
+		if (commentRating != other.commentRating)
+			return false;
+		if (commentTime == null) {
+			if (other.commentTime != null)
+				return false;
+		} else if (!commentTime.equals(other.commentTime))
+			return false;
+		if (houseSerial != other.houseSerial)
+			return false;
+		if (memberEamil == null) {
+			if (other.memberEamil != null)
+				return false;
+		} else if (!memberEamil.equals(other.memberEamil))
+			return false;
+		return true;
+	}
+	
+	
 }
--- Perplus/src/com/perplus/member/vo/HouseZzimVo.java
+++ Perplus/src/com/perplus/member/vo/HouseZzimVo.java
@@ -1,5 +1,56 @@
 package com.perplus.member.vo;
 
 public class HouseZzimVo {
-
+	private int houseSerial;
+	private String memberEmail;
+	public HouseZzimVo(int houseSerial, String memberEmail) {
+		this.houseSerial = houseSerial;
+		this.memberEmail = memberEmail;
+	}
+	public HouseZzimVo() {
+	}
+	public int getHouseSerial() {
+		return houseSerial;
+	}
+	public void setHouseSerial(int houseSerial) {
+		this.houseSerial = houseSerial;
+	}
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	@Override
+	public String toString() {
+		return "HouseZzimVo [houseSerial=" + houseSerial + ", memberEmail=" + memberEmail + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + houseSerial;
+		result = prime * result + ((memberEmail == null) ? 0 : memberEmail.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HouseZzimVo other = (HouseZzimVo) obj;
+		if (houseSerial != other.houseSerial)
+			return false;
+		if (memberEmail == null) {
+			if (other.memberEmail != null)
+				return false;
+		} else if (!memberEmail.equals(other.memberEmail))
+			return false;
+		return true;
+	}
+	
+	
 }
