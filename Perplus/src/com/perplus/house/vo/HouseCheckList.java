384fa57b520fbbe334964e8b70537d318ea9f498
hojin Lee <lhj8770@gmail.com>
2016-12-01 16:51:50 +0900

    houseCheckList 테이블 추가 작업입니다.

---
+++ Perplus/src/com/perplus/house/vo/HouseCheckList.java
@@ -0,0 +1,71 @@
+package com.perplus.house.vo;
+
+import java.io.Serializable;
+
+public class HouseCheckList implements Serializable{
+	private int houseSerial;
+	private String list;
+	private String value;
+	public HouseCheckList(int houseSerial, String list, String value) {
+		super();
+		this.houseSerial = houseSerial;
+		this.list = list;
+		this.value = value;
+	}
+	public int getHouseSerial() {
+		return houseSerial;
+	}
+	public void setHouseSerial(int houseSerial) {
+		this.houseSerial = houseSerial;
+	}
+	public String getList() {
+		return list;
+	}
+	public void setList(String list) {
+		this.list = list;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	@Override
+	public String toString() {
+		return "HouseCheckList [houseSerial=" + houseSerial + ", list=" + list + ", value=" + value + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + houseSerial;
+		result = prime * result + ((list == null) ? 0 : list.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HouseCheckList other = (HouseCheckList) obj;
+		if (houseSerial != other.houseSerial)
+			return false;
+		if (list == null) {
+			if (other.list != null)
+				return false;
+		} else if (!list.equals(other.list))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+	
+	
+}
