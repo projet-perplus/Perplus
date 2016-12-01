c9b76ef82eba4f62b1067a54fa046ea0cbab000e
Jeong Sae Han Seul <bluehansl@naver.com>
2016-12-01 12:14:14 +0900

    TravelDao insert SQL구문

--- Perplus/src/com/perplus/member/dao/TravelDao.java
+++ Perplus/src/com/perplus/member/dao/TravelDao.java
@@ -1,5 +1,16 @@
 package com.perplus.member.dao;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.perplus.member.vo.TravelVo;
+
 public interface TravelDao {
 
+	/**
+	 * Travel 객체 insert
+	 * @param session
+	 * @param travel
+	 * @return
+	 */
+	int insertTravel(SqlSession session, TravelVo travel);
 }
