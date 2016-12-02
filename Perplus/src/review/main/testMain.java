4aa6996557356392134507c2bb667065427a77fc
skfo9949 <skfo9949@naver.com>
2016-12-02 17:18:36 +0900

    sss

---
+++ Perplus/src/review/main/testMain.java
@@ -0,0 +1,18 @@
+package review.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.perplus.review.daoimpl.ReviewCommentDaoImpl;
+import com.perplus.review.daoimpl.ReviewDaoImpl;
+import com.perplus.review.daoimpl.ReviewPictureDaoImpl;
+
+public class testMain {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext abc = new ClassPathXmlApplicationContext("test-mvc.xml");
+		ReviewDaoImpl r = (ReviewDaoImpl)abc.getBean("reviewDaoImpl");
+		ReviewCommentDaoImpl c = (ReviewCommentDaoImpl)abc.getBean("reviewCommentDaoImpl");
+		ReviewPictureDaoImpl p =(ReviewPictureDaoImpl)abc.getBean("reviewPictureDaoImpl");
+		
+		System.out.println(r.selectReviewWithCommentAndPicture(1));
+	}
+}
