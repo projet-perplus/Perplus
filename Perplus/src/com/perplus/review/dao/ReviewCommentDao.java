c0dcc41cf53a04b31ba00c7ff511b0fecffcd83d
skfo9949 <skfo9949@naver.com>
2016-12-02 15:41:41 +0900

    review

--- Perplus/src/com/perplus/review/dao/ReviewCommentDao.java
+++ Perplus/src/com/perplus/review/dao/ReviewCommentDao.java
@@ -1,17 +1,19 @@
 package com.perplus.review.dao;
 
+import java.util.List;
+
 import com.perplus.review.vo.ReviewCommentVo;
 
 public interface ReviewCommentDao {
 	//리뷰 댓글 insert하는 메소드
 	int insertReviewComment(ReviewCommentVo reviewComment);
-/*	//리뷰 serial / 댓글 등록한 회원 email을 통해서 댓글 조회하는 메소드
-	ReviewCommentVo selectReviewCommentBySerialAndEmail(int reviewSerial, String memberEmail);*/
-	//리뷰 serial을 통해 댓글 조회
-	ReviewCommentVo selectReviewCommentBySerial(int reviewSerial);
+	//리뷰 serial / 댓글 등록한 회원 email을 통해서 댓글 조회하는 메소드
+	ReviewCommentVo selectReviewCommentBySerialAndEmail(int reviewSerial, String memberEmail);
+	//리뷰 serial을 통해 댓글전체 조회
+	List<ReviewCommentVo> selectReviewCommentListBySerial(int reviewSerial);
 	//리뷰 serial / 댓글 등록한 회원 email을 통해서 댓글 수정
 	int updateReviewComment(ReviewCommentVo reviewComment);
 	//리뷰 serial / 댓글 등록한 회원 email을 통해서 댓글 삭제
-	int deleteReviewComment(ReviewCommentVo reviewComment);
+	int deleteReviewComment(int reviewSerial,String memberEmail);
 	
 }
--- Perplus/src/com/perplus/review/daoimpl/ReviewCommentDaoImpl.java
+++ Perplus/src/com/perplus/review/daoimpl/ReviewCommentDaoImpl.java
@@ -1,5 +1,9 @@
 package com.perplus.review.daoimpl;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -12,34 +16,50 @@ public class ReviewCommentDaoImpl implements ReviewCommentDao{
 
 	@Autowired
 	private SqlSessionTemplate session;
-	
+	/**
+	 * 리뷰 코멘트 등록하는 메소드
+	 */
 	@Override
 	public int insertReviewComment(ReviewCommentVo reviewComment) {
 		return session.insert("reviewComment.insertComment", reviewComment);
 	}
-
-/*	@Override
-	public ReviewCommentVo selectReviewCommentBySerialAndEmail(int reviewSerial, String memberEmail) {
-		// TODO Auto-generated method stub
-		return null;
-	}*/
-
+	/**
+	 * 내가 작성한 comment select하는 메소드
+	 */
 	@Override
-	public ReviewCommentVo selectReviewCommentBySerial(int reviewSerial) {
-		// TODO Auto-generated method stub
-		return null;
+	public ReviewCommentVo selectReviewCommentBySerialAndEmail(int reviewSerial, String memberEmail) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("reviewSerial", reviewSerial);
+		map.put("memberEmail", memberEmail);
+		return session.selectOne("reviewComment.selectMyComment", map);
 	}
 
+	/**
+	 * 해당 리뷰에 있는 모든 comment select하는 메소드
+	 */
+	@Override
+	public List<ReviewCommentVo> selectReviewCommentListBySerial(int reviewSerial) {
+		return session.selectList("reviewComment.selectCommentList", reviewSerial);
+	}
+
+	/**
+	 * 이메일과 리뷰serial을 통해 comment 수정하는 메소드
+	 */
 	@Override
 	public int updateReviewComment(ReviewCommentVo reviewComment) {
-		// TODO Auto-generated method stub
-		return 0;
+		return session.update("reviewComment.updateComment",reviewComment);
+	}
+	/**
+	 * reviewSerial과 memberEmail을 통해 review 삭제하는 메소드.
+	 */
+	@Override
+	public int deleteReviewComment(int reviewSerial, String memberEmail) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("reviewSerial", reviewSerial);
+		map.put("memberEmail", memberEmail);
+		return session.delete("reviewComment.deleteComment", map);
 	}
 
-	@Override
-	public int deleteReviewComment(ReviewCommentVo reviewComment) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+
 	
 }
