8aaa4ec18ed0f1dcc734e0b2d0cdf6efb4e0d098
hojin Lee <lhj8770@gmail.com>
2016-12-05 15:03:28 +0900

    commit

--- Perplus/src/com/perplus/house/serviceimpl/HouseServiceImpl.java
+++ Perplus/src/com/perplus/house/serviceimpl/HouseServiceImpl.java
@@ -1,5 +1,54 @@
 package com.perplus.house.serviceimpl;
 
-public class HouseServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.perplus.house.daoimpl.HouseFilterDaoImpl;
+import com.perplus.house.service.HouseService;
+import com.perplus.house.vo.HouseFilterVo;
+
+@Service
+public class HouseServiceImpl implements HouseService{
+
+	@Autowired
+	private HouseFilterDaoImpl houseFilterDao;
+
+	//house service
+	//housefilter service
+	
+	@Override
+	public void insertHouseFilter(HouseFilterVo houseFilter) {
+		// TODO Auto-generated method stub
+		houseFilterDao.insertHouseFilter(houseFilter);
+	}
+
+	@Override
+	public void deleteHouseFilterByHouseSerial(int houseSerial) {
+		// TODO Auto-generated method stub
+		houseFilterDao.deleteHouseFilterByHouseSerial(houseSerial);
+	}
+
+	@Override
+	public HouseFilterVo selectHouseFilterByHouseSerial(int houseSerial) {
+		// TODO Auto-generated method stub
+		return houseFilterDao.selectHouseFilterByHouseSerial(houseSerial);
+	}
+
+	@Override
+	public HouseFilterVo selectHouseFilterByHouseSerialWithJoin(int houseSerial) {
+		return houseFilterDao.selectHouseFilterByHouseSerialWithJoin(houseSerial);
+	}
+
+	@Override
+	public void updateHouseFilter(HouseFilterVo houseFilter) {
+		houseFilterDao.updateHouseFilter(houseFilter);
+	}
+	
+	//checklist service
+	
+	//shutdown service
+	
+	//housePicture service
+	
+	//codetable service
 }
