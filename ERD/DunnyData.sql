cbf442a638a988f069fb1e14a8aba406424ea43a
Jeong Sae Han Seul <bluehansl@naver.com>
2016-12-05 10:06:23 +0900

    tableDummy

---
+++ ERD/DunnyData.sql
@@ -0,0 +1,9 @@
+insert into HOUSE values(1,'bbb',0,'따뜻한 집','조흔 집입니다1','ㅇㅇㅇ',null,null,0,0,0,0,0);
+insert into HOUSE values(2,'bbb',0,'추운 즵','조흔 집입니다2','ㅇㅇㅇ',null,null,0,0,0,0,0);
+insert into HOUSE values(3,'bbb',0,'초가집','조흔 집입니다3','ㅇㅇㅇ',null,null,0,0,0,0,0);
+insert into HOUSE values(4,'bbb',0,'이글루','조흔 집입니다4','ㅇㅇㅇ',null,null,0,0,0,0,0);
+
+insert into MEMBER values('aaa','wegwe','김아무개','199ㄷ-02-04','남',01023,'경기',null,0,null);
+insert into MEMBER values('bbb','dsgwe','아아무개','1993-02-04','남',01023,'경기',null,0,null);
+insert into MEMBER values('ccc','hfgwe','중아무개','199ㄹ-02-04','남',01023,'경기',null,0,null);
+insert into MEMBER values('ddd','xcgwe','이아무개','199ㅍ-02-04','남',01023,'경기',null,0,null);
