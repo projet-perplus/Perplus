956839a6e711ea310eefd20f39a752e6a54170a1
hojin Lee <lhj8770@gmail.com>
2016-12-12 21:05:26 +0900

    commit

--- Perplus/WebContent/etc/sequence.sql
+++ Perplus/WebContent/etc/sequence.sql
@@ -2,12 +2,14 @@ create sequence HOUSE_SEQ increment by 1;
 create sequence CHECKLIST_SEQ increment by 1;
 create sequence HOUSEPICTURE_SEQ increment by 1;
 create sequence SHUTDOWN_SEQ increment by 1;
+create sequence REVIEW_SEQ increment by 1;
 
 
 drop sequence HOUSE_SEQ;
 drop sequence CHECKLIST_SEQ;
 drop sequence HOUSEPICTURE_SEQ;
 drop sequence SHUTDOWN_SEQ;
+drop sequence REVIEW_SEQ;
 
 alter sequence HOUSE_SEQ increment by 1;
 select HOUSE_SEQ.nextval from DUAL;
